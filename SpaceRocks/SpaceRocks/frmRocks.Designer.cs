f5a8c867b90541e8dc0426f6d93c2619ade193e6
Cody Silby <codysilby@mmc.school.nz>
2019-06-20 12:25:53 +1200

    Added GUI features - header, background, images for use later on

---
+++ SpaceRocks/SpaceRocks/frmRocks.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace SpaceRocks
+{
+    partial class frmRocks
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblLives = new System.Windows.Forms.Label();
+            this.Heading = new System.Windows.Forms.PictureBox();
+            this.pnlGame = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.Heading)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Score";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Lives";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(686, 14);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = " Let\'s Go!";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(686, 46);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "Let\'s Stop!";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(59, 23);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(13, 13);
+            this.lblScore.TabIndex = 6;
+            this.lblScore.Text = "0";
+            // 
+            // lblLives
+            // 
+            this.lblLives.AutoSize = true;
+            this.lblLives.Location = new System.Drawing.Point(55, 56);
+            this.lblLives.Name = "lblLives";
+            this.lblLives.Size = new System.Drawing.Size(13, 13);
+            this.lblLives.TabIndex = 7;
+            this.lblLives.Text = "5";
+            // 
+            // Heading
+            // 
+            this.Heading.Image = global::SpaceRocks.Properties.Resources.header;
+            this.Heading.Location = new System.Drawing.Point(110, 4);
+            this.Heading.Name = "Heading";
+            this.Heading.Size = new System.Drawing.Size(560, 90);
+            this.Heading.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Heading.TabIndex = 1;
+            this.Heading.TabStop = false;
+            // 
+            // pnlGame
+            // 
+            this.pnlGame.BackgroundImage = global::SpaceRocks.Properties.Resources.background;
+            this.pnlGame.Location = new System.Drawing.Point(12, 100);
+            this.pnlGame.Name = "pnlGame";
+            this.pnlGame.Size = new System.Drawing.Size(760, 449);
+            this.pnlGame.TabIndex = 0;
+            // 
+            // frmRocks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.lblLives);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Heading);
+            this.Controls.Add(this.pnlGame);
+            this.Name = "frmRocks";
+            this.Text = "SpaceRocks";
+            this.Load += new System.EventHandler(this.frmRocks_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Heading)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlGame;
+        private System.Windows.Forms.PictureBox Heading;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblLives;
+    }
+}
+
